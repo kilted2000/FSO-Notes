70ed029efd6b800a726d3a86006a5d5e5b254a99
Richard <richardlwhittington@gmail.com>
2026-03-09 00:09:55 -0500

    fix dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-    server: {
+  server: command === 'serve' ? {
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
       },
     }
-  },
-
-})
+  } : {},
+}))
